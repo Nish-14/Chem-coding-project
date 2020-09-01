b58d32787f3ac2721768d294df4d947d0e414062
Nish-14 <69695306+Nish-14@users.noreply.github.com>
2020-09-01 14:57:40 -0700

    Add files via upload

---
+++ Main (2).java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner reader = new Scanner(System.in);
+		System.out.println("Enter in the mass of water, in grams");
+		double mass = reader.nextDouble();
+		System.out.println("Enter the initial temperature, in Celsius");
+		double initialTemp = reader.nextDouble();
+		if(initialTemp < -273.14)
+	    	initialTemp= -273.14;
+		System.out.println("Enter the final temperature, in Celsius");
+		double finalTemp = reader.nextDouble();
+		if(finalTemp < -273.14)
+	    	finalTemp = -273.14;
+	    	
+	    System.out.println("Mass:" + mass + "g");
+	    System.out.println("Initial temperature:" + initialTemp + "C");
+	    System.out.println("Final temperature:" + finalTemp + "g");
+		
+	}
+}
